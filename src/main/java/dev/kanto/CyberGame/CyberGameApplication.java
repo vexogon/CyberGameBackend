2cb7a346b9909878c5bfa60bb512496b9a358aa7
VexoGon <68669016+VexoGon@users.noreply.github.com>
2022-05-24 23:00:49 +0100

    Target Added

--- src/main/java/dev/kanto/CyberGame/CyberGameApplication.java
+++ src/main/java/dev/kanto/CyberGame/CyberGameApplication.java
@@ -1,11 +1,9 @@
 package dev.kanto.CyberGame;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class CyberGameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CyberGameApplication.class, args);
